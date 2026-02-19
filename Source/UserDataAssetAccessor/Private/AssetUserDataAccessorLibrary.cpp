bd92ebec566659728e8feb4b6e607e50904411df
MaxEstLa <96690800+MaxEstDev@users.noreply.github.com>
2026-02-19 13:59:01 +0100

    initial commit

---
+++ Source/UserDataAssetAccessor/Private/AssetUserDataAccessorLibrary.cpp
@@ -0,0 +1,68 @@
+﻿// MaxEstLa - Blood Rush - ALL RIGHTS RESERVED 2022-2026
+
+
+#include "AssetUserDataAccessorLibrary.h"
+#include "Interfaces/Interface_AssetUserData.h"
+
+bool UAssetUserDataAccessorLibrary::ImplementsAssetUserData(UObject* Object)
+{
+	return IsValid(GetUserDataInterface(Object)->_getUObject());
+}
+
+void UAssetUserDataAccessorLibrary::AddAssetUserData(UObject* Object, UAssetUserData* AssetUserData)
+{
+	IInterface_AssetUserData* Interface = GetUserDataInterface(Object);
+	if (Interface)
+	{
+		Interface->AddAssetUserData(AssetUserData);
+	}
+}
+
+void UAssetUserDataAccessorLibrary::RemoveAssetUserData(UObject* Object, const TSubclassOf<UAssetUserData> AssetUserDataClass)
+{
+	IInterface_AssetUserData* Interface = GetUserDataInterface(Object);
+	if (Interface)
+	{
+		Interface->RemoveUserDataOfClass(AssetUserDataClass);
+	}
+}
+
+void UAssetUserDataAccessorLibrary::RemoveAllAssetUserData(UObject* Object)
+{
+	RemoveAssetUserData(Object,UAssetUserData::StaticClass());
+}
+
+bool UAssetUserDataAccessorLibrary::HasAnyAssetUserData(UObject* Object)
+{
+	const TSubclassOf<UAssetUserData> ParentClass = UAssetUserData::StaticClass();
+	UAssetUserData* ReturnValue = nullptr;
+	return GetAssetUserDataByClass(Object,ParentClass,ReturnValue);
+}
+
+bool UAssetUserDataAccessorLibrary::GetAssetUserDataByClass(UObject* Object, const TSubclassOf<UAssetUserData> Class, UAssetUserData*& AssetUserData)
+{
+	if (IInterface_AssetUserData* Interface = GetUserDataInterface(Object))
+	{
+		AssetUserData = Interface->GetAssetUserDataOfClass(Class);
+		return IsValid(AssetUserData);
+	}
+	
+	return false;
+}
+
+TArray<UAssetUserData*> UAssetUserDataAccessorLibrary::GetAssetUserDataArray(UObject* Object)
+{
+	if (IInterface_AssetUserData* Interface = GetUserDataInterface(Object))
+	{
+		const TArray<UAssetUserData*>* AssetUserDataArray = nullptr;
+		AssetUserDataArray = Interface->GetAssetUserDataArray();
+		return AssetUserDataArray ? *AssetUserDataArray : TArray<UAssetUserData*>();
+	}
+	
+	return TArray<UAssetUserData*>();
+}
+
+IInterface_AssetUserData* UAssetUserDataAccessorLibrary::GetUserDataInterface(UObject* Object)
+{
+	return Cast<IInterface_AssetUserData>(Object);
+}
---
+++ Source/UserDataAssetAccessor/Public/AssetUserDataAccessorLibrary.h
@@ -0,0 +1,45 @@
+﻿// MaxEstLa - Blood Rush - ALL RIGHTS RESERVED 2022-2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "AssetUserDataAccessorLibrary.generated.h"
+
+/**
+ * This library allows you to use the Interface_AssetUserData class
+ * This interface is not properly implemented in all classes in the engine
+ * Please check its implementation prior to using it
+ * i.e UActorComponent doesn't implement GetAssetUserDataArray() but implements other methods (WHY???)
+ */
+UCLASS()
+class USERDATAASSETACCESSOR_API UAssetUserDataAccessorLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	public:
+	
+	UFUNCTION(BlueprintCallable, BlueprintPure)
+	static bool ImplementsAssetUserData(UObject* Object);
+	
+	UFUNCTION(BlueprintCallable)
+	static void AddAssetUserData(UObject* Object, UAssetUserData* AssetUserData);
+	
+	UFUNCTION(BlueprintCallable)
+	static void RemoveAssetUserData(UObject* Object, const TSubclassOf<UAssetUserData> AssetUserDataClass);
+	
+	UFUNCTION(BlueprintCallable)
+	static void RemoveAllAssetUserData(UObject* Object);
+	
+	UFUNCTION(BlueprintCallable)
+	static bool HasAnyAssetUserData(UObject* Object);
+	
+	UFUNCTION(BlueprintCallable, meta=(DeterminesOutputType="Class", DynamicOutputParam="AssetUserData"))
+	static bool GetAssetUserDataByClass(UObject* Object, const TSubclassOf<UAssetUserData> Class, UAssetUserData*& AssetUserData);
+	
+	UFUNCTION(BlueprintCallable)
+	static TArray<UAssetUserData*> GetAssetUserDataArray(UObject* Object);
+	
+private:
+	static IInterface_AssetUserData* GetUserDataInterface(UObject* Object);
+};
