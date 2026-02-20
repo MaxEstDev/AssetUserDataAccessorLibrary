99a8b47b7fc265aaa99ca87d9742728a96f8f5a5
MaxEstLa <96690800+MaxEstDev@users.noreply.github.com>
2026-02-20 18:12:23 +0100

    added blueprintable base class for BP only users

---
+++ Source/UserDataAssetAccessor/Private/AssetUserDataBase.cpp
@@ -0,0 +1,4 @@
+﻿// MaxEstLa - Blood Rush - ALL RIGHTS RESERVED 2022-2026
+
+
+#include "AssetUserDataBase.h"
---
+++ Source/UserDataAssetAccessor/Public/AssetUserDataBase.h
@@ -0,0 +1,16 @@
+﻿// MaxEstLa - Blood Rush - ALL RIGHTS RESERVED 2022-2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetUserData.h"
+#include "AssetUserDataBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType)
+class USERDATAASSETACCESSOR_API UAssetUserDataBase : public UAssetUserData
+{
+	GENERATED_BODY()
+};
